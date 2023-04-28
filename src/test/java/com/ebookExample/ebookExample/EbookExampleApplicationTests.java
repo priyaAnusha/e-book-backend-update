18e93649e3afef4d5a2c53bc0061a49f98f4b66b
priya anusha <priyaanusha564@gmail.com>
2023-04-28 11:32:15 +0530

    changed annotation in test

--- src/test/java/com/ebookExample/ebookExample/EbookExampleApplicationTests.java
+++ src/test/java/com/ebookExample/ebookExample/EbookExampleApplicationTests.java
@@ -3,7 +3,9 @@ package com.ebookExample.ebookExample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.ebook.EbookExampleApplication;
+
+@SpringBootTest(classes = EbookExampleApplication.class)
 class EbookExampleApplicationTests {
 
 	@Test
